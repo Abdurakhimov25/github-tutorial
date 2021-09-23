b3a3a2d6e5060dc1a8901f9c2873e3a21aa38abb
Abdurakhimov25 <abdurakhimov2001@gmail.com>
2021-09-23 12:01:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let p;
+// let a=6;
+// let b=5;
+// let c=3;
+// p=Math.min(a,b,c);
+// console.log(p)
+// const letter='salom DD<DDMSD dunyo';
+// console.log(letter.toLowerCase())
+
+let firstName=prompt('Yoshingiz nechada',"");
+console.log(firstName)
+
+console.log(`salom dunyo'' ${firstName}`)
